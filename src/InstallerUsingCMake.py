337c71e6af6755997be0f385646eef2f307c774f
loeiten <michael.l.magnussen@gmail.com>
2018-07-12 11:01:28 +0200

    Bugfix: make is now called from build

--- src/InstallerUsingCMake.py
+++ src/InstallerUsingCMake.py
@@ -10,7 +10,11 @@ class InstallerUsingCMake(Installer):
     @staticmethod
     def get_cmake_command(cmake_options=None):
         """
-        Get the command to configure the package
+        Get the command to cmake the package
+
+        Notes
+        -----
+        The `..` at the end of cmake is automatically appended
 
         Parameters
         ----------
@@ -30,21 +34,21 @@ class InstallerUsingCMake(Installer):
             for key, val in cmake_options.items():
                 options += f' -{key}={val}'
 
-        cmake_str = f'cmake{options}'
+        cmake_str = f'cmake{options} ..'
         return cmake_str
 
-    def run_configure(self,
-                      tar_dir,
-                      makefile_path,
-                      extra_cmake_option,
-                      overwrite_on_exist):
+    def run_cmake(self,
+                  build_dir,
+                  makefile_path,
+                  extra_cmake_option,
+                  overwrite_on_exist):
         """
         Configures the package by running CMake
 
         Parameters
         ----------
-        tar_dir : Path
-            Directory of the tar file
+        build_dir : Path
+            Directory to make the build
         makefile_path : Path
             Path to the Makefile
         extra_cmake_option:
@@ -62,7 +66,6 @@ class InstallerUsingCMake(Installer):
 
             cmake_str = self.get_cmake_command(cmake_options=cmake_options)
 
-            build_dir = tar_dir.joinpath('build')
             # NOTE: We could get in trouble if the build_dir already exists,
             #       so we deliberately throw an error if the directory exists
             build_dir.mkdir(exist_ok=False)
@@ -72,3 +75,43 @@ class InstallerUsingCMake(Installer):
         else:
             self.logger.info(f'{makefile_path} found, skipping running of '
                              f'CMake')
+
+    def install_package(self,
+                        url,
+                        file_from_make,
+                        overwrite_on_exist=False,
+                        extra_cmake_option=None):
+        """
+        Installs a package if it's not installed
+
+        Parameters
+        ----------
+        url : str
+            Url to the tar file of the package
+        file_from_make : Path or str
+            File originating from the make processes (used to check if the
+            package has been made)
+        overwrite_on_exist : bool
+            Whether to overwrite the package if it is already found
+        extra_cmake_option : dict
+            Configure option to include.
+            The installation prefix of self.local_dir is already added as an
+            option
+        """
+
+        # Download the tar file
+        tar_file_path = self.get_tar_file_path(url)
+        self.run_download_tar(url, tar_file_path, overwrite_on_exist)
+
+        # Untar
+        tar_dir = self.get_tar_dir(tar_file_path)
+        self.run_untar(tar_file_path, tar_dir, overwrite_on_exist)
+
+        # Configure and make
+        build_dir = tar_dir.joinpath('build')
+        makefile_path = build_dir.joinpath('build', 'Makefile')
+        self.run_cmake(build_dir,
+                       makefile_path,
+                       extra_cmake_option,
+                       overwrite_on_exist)
+        self.run_make(build_dir, file_from_make, overwrite_on_exist)
