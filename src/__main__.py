5c8721932ba6674cb442633039feee36f3fb49b3
loeiten <michael.l.magnussen@gmail.com>
2018-11-12 17:05:29 +0100

    Updates to main

--- src/__main__.py
+++ src/__main__.py
@@ -4,7 +4,7 @@
 import argparse
 import configparser
 from pathlib import Path
-# from src.BOUTPPInstaller import BOUTPPInstaller
+from src.BOUTPPInstaller import BOUTPPInstaller
 from src.CMakeInstaller import CMakeInstaller
 from src.FFMPEGInstaller import FFMPEGInstaller
 from src.FFTWInstaller import FFTWInstaller
@@ -17,7 +17,7 @@ from src.SLEPcInstaller import SLEPcInstaller
 from src.SundialsInstaller import SundialsInstaller
 
 
-def main(config_path):
+def main(config_path, add_to_bashrc=False):
     """
     Function which installs BOUT++ with its dependencies.
 
@@ -26,20 +26,29 @@ def main(config_path):
     config_path : str or Path
         Path to the configuration file.
         See src.config.ini for the default configuration
+    add_to_bashrc : bool
+        Whether or not to add binaries and library path of the dependencies
+        to .bashrc
     """
 
     # FIXME: Options for adding lines to bashrc or bash_profile
-    # FIXME: Why is netcdf_cxx in config.ini?
+
+    # String to print when installation is complete
+    final_str = '\n\n'
 
     config = configparser.ConfigParser(allow_no_value=True)
     with config_path.open() as f:
         config.read_file(f)
 
     if config.getboolean('optional', 'gcc'):
-        # FIXME: Must have in bashrc or bash_profile
         print('Installing gcc...')
         gcc_installer = GCCInstaller(config_path=config_path)
         gcc_installer.install()
+        final_str += (f'export PATH="'
+                      f'{gcc_installer.local_dir.joinpath("bin")}:$PATH"\n')
+        final_str += (f'export LD_LIBRARY_PATH = '
+                      f'{gcc_installer.local_dir.joinpath("lib64")}:'
+                      f'$LD_LIBRARY_PATH"\n')
         print('..done')
 
     if config.getboolean('required', 'mpi'):
@@ -96,9 +105,39 @@ def main(config_path):
         slepc_installer.install()
         print('...done')
 
-#    boutpp_installer = BOUTPPInstaller(config_path=config_path)
-#    boutpp_installer.install()
-#    print('...done')
+    boutpp_installer = BOUTPPInstaller(config_path=config_path)
+    boutpp_installer.install()
+    print('...done')
+
+    final_str += (f'export LD_LIBRARY_PATH='
+                  f'{boutpp_installer.local_dir.joinpath("lib")}:'
+                  f'LD_LIBRARY_PATH')
+
+    if add_to_bashrc:
+        add_str_to_bashrc(final_str)
+    else:
+        print('Make sure that all binaries and libraries are in the PATH')
+        print('You can do so by making sure that the following is in .bashrc:\n')
+        print(final_str)
+
+
+def add_str_to_bashrc(bashrc_str):
+    """
+    Adds the bashrc_str to .bashrc
+
+    Parameters
+    ----------
+    bashrc_str : str
+        The string to add to bashrc
+    """
+
+    bashrc_str = f'# Added by bout_installer.py{bashrc_str}'
+    bashrc_path = Path.home().joinpath('.bashrc')
+
+    print(f'Adding following to .bashrc:\n{bashrc_str}')
+
+    with bashrc_path.open('a') as f:
+        f.write(bashrc_str)
 
 
 if __name__ == '__main__':
@@ -110,13 +149,24 @@ if __name__ == '__main__':
                         help='Path to the configuration file. See '
                              'src/config.ini for details',
                         required=False)
+    parser.add_argument('-a',
+                        '--add_to_bashrc',
+                        help='If set, paths to binaries and libraries of '
+                             'dependencies will be added to .bashrc',
+                        action='store_true',
+                        required=False)
 
     args = parser.parse_args()
 
     if args.config is None:
         root_dir = Path(__file__).absolute().parents[1]
-        config_path = root_dir.joinpath('src', 'config.ini')
+        config_path_ = root_dir.joinpath('src', 'config.ini')
     else:
-        config_path = Path(args.config)
+        config_path_ = Path(args.config)
 
-    main(config_path)
+    if args.add_to_bashrc:
+        add_to_bashrc_ = True
+    else:
+        add_to_bashrc_ = False
+
+    main(config_path_, add_to_bashrc=add_to_bashrc_)
