d96cdaaf5c15e43b6a64e0cc9b127be8baa85567
loeiten <michael.l.magnussen@gmail.com>
2019-10-26 23:32:14 +0200

    Testing travis with docker only

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -9,5 +9,7 @@ docker build -f docker/Dockerfile -t loeiten/fruit_classifier:latest .
 #       https://docs.travis-ci.com/user/docker/#building-a-docker-image-from-a-dockerfile
 #       for detaills
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-VERSION=`grep -E "^__version__ = ['\"]([^'\"]*)['\"]" __init__.py | grep -oEi "\d+\.\d+\.\d+"`
+VERSION=`sed -n "s/__version__ = ['\"]\([^'\"]*\)['\"]/\1/p" __init__.py`_$(date +%Y%m%d)
 docker push loeiten/fruit_classifier:$(VERSION)
+docker tag loeiten/fruit_classifier:$(VERSION) loeiten/fruit_classifier:latest
+docker push loeiten/fruit_classifier:latest
