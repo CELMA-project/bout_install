4478d643b2b618fb96641c0f5111d83a1224842a
Michael Løiten <michael.l.magnussen@gmail.com>
2019-10-27 12:15:37 +0100

    Fixes to travis and docker

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-docker build -f docker/Dockerfile -t loeiten/fruit_classifier:latest .
+VERSION=$(sed -n "s/__version__ = ['\"]\([^'\"]*\)['\"]/\1/p" bout_install/__init__.py)_$(date +%Y%m%d)
+docker build -f docker/Dockerfile -t loeiten/fruit_classifier:"$VERSION" .
 # NOTE: It appears that DOCKER_PASSWORD and DOCKER_USERNAME cannot be
 #       set as an travis environment variable. Instead it can be set by
 #       travis env set DOCKER_USERNAME myusername
@@ -9,7 +10,6 @@ docker build -f docker/Dockerfile -t loeiten/fruit_classifier:latest .
 #       https://docs.travis-ci.com/user/docker/#building-a-docker-image-from-a-dockerfile
 #       for detaills
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-VERSION=`sed -n "s/__version__ = ['\"]\([^'\"]*\)['\"]/\1/p" __init__.py`_$(date +%Y%m%d)
-docker push loeiten/fruit_classifier:$(VERSION)
-docker tag loeiten/fruit_classifier:$(VERSION) loeiten/fruit_classifier:latest
+docker push loeiten/fruit_classifier:"$VERSION"
+docker tag loeiten/fruit_classifier:"$VERSION" loeiten/fruit_classifier:latest
 docker push loeiten/fruit_classifier:latest
